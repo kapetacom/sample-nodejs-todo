7d020e250ca480f97ae6138660de2926a8ec7f65
Henrik Hofmeister <henrik@newdawn.dk>
2023-08-24 16:35:30 +0200

    chore: Clean up before creating tar

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,12 @@
 echo "Installing Kapeta CLI..."
 npm i -g @kapeta/kap || exit 1
 
+REPO_HOME="$HOME/.kapeta/repository"
+
+if [ -d "$REPO_HOME" ]; then
+  rm -rf "${REPO_HOME}/*" # Remove old versions
+fi
+
 echo "Installing latest version of plan..."
 kap registry install -n kapeta/sample-nodejs-plan || exit 1
 
@@ -14,7 +20,7 @@ if [ -f "$CURRENT_DIR/$TARFILE" ]; then
   rm "$CURRENT_DIR/$TARFILE"
 fi
 
-cd $HOME/.kapeta/repository/ || exit 1
+cd "$REPO_HOME" || exit 1
 tar -cvzf "$CURRENT_DIR/$TARFILE" kapeta/* || exit 1
 cd "$CURRENT_DIR" || exit 1
 echo "Tar file created: $TARFILE"
