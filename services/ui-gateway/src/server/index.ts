b67d7594f363f90216fc29b8711b39b4edaaa460
andertun <harald.andertun@gmail.com>
2023-09-12 12:49:53 +0200

    fix: Remove unused imports

--- services/ui-gateway/src/server/index.ts
+++ services/ui-gateway/src/server/index.ts
@@ -1,28 +1,20 @@
-import Path from "path";
-import FS from "fs";
-import { Server } from "@kapeta/sdk-server";
-const server = new Server(
-    "kapeta/sample-nodejs-ui-gateway",
-    Path.resolve(__dirname, "../..")
-);
-import express from "express";
-import history from "connect-history-api-fallback";
-import { UsersProxyRoute } from "./proxies/rest/UsersProxyRoute";
-import { TodowebProxyRoute } from "./proxies/fragments/TodowebProxyRoute";
+import Path from 'path';
+import { Server } from '@kapeta/sdk-server';
+const server = new Server('kapeta/sample-nodejs-ui-gateway', Path.resolve(__dirname, '../..'));
+import { UsersProxyRoute } from './proxies/rest/UsersProxyRoute';
+import { TodowebProxyRoute } from './proxies/fragments/TodowebProxyRoute';
 
-import { UserwebProxyRoute } from "./proxies/fragments/UserwebProxyRoute";
+import { UserwebProxyRoute } from './proxies/fragments/UserwebProxyRoute';
 
-const devMode =
-    process.env.NODE_ENV &&
-    process.env.NODE_ENV.toLowerCase() === "development";
+const devMode = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'development';
 
 server.addRoute(new TodowebProxyRoute());
 
 server.addRoute(new UserwebProxyRoute());
 server.addRoute(new UsersProxyRoute());
 
-const BASE_DIR = Path.resolve(__dirname, "../../dist");
-const webpackConfig = require("../../webpack.development.config");
+const BASE_DIR = Path.resolve(__dirname, '../../dist');
+const webpackConfig = require('../../webpack.development.config');
 server.configureAssets(BASE_DIR, webpackConfig);
 
-server.start("web");
+server.start('web');
--- services/users-ui/src/server/index.ts
+++ services/users-ui/src/server/index.ts
@@ -1,22 +1,14 @@
-import Path from "path";
-import FS from "fs";
-import { Server } from "@kapeta/sdk-server";
-const server = new Server(
-    "kapeta/nodejs-sample-user-ui",
-    Path.resolve(__dirname, "../..")
-);
-import express from "express";
-import history from "connect-history-api-fallback";
-import { UsersProxyRoute } from "./proxies/rest/UsersProxyRoute";
+import Path from 'path';
+import { Server } from '@kapeta/sdk-server';
+const server = new Server('kapeta/nodejs-sample-user-ui', Path.resolve(__dirname, '../..'));
+import { UsersProxyRoute } from './proxies/rest/UsersProxyRoute';
 
-const devMode =
-    process.env.NODE_ENV &&
-    process.env.NODE_ENV.toLowerCase() === "development";
+const devMode = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'development';
 
 server.addRoute(new UsersProxyRoute());
 
-const BASE_DIR = Path.resolve(__dirname, "../../dist");
-const webpackConfig = require("../../webpack.development.config");
+const BASE_DIR = Path.resolve(__dirname, '../../dist');
+const webpackConfig = require('../../webpack.development.config');
 server.configureAssets(BASE_DIR, webpackConfig);
 
-server.start("web");
+server.start('web');
