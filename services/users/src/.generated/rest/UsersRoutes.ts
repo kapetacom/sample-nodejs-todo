4e06231780b97c20ceb6cd69fd346769d54d4537
Henrik Hofmeister <henrik@newdawn.dk>
2024-01-09 20:10:07 +0100

    feat: Bumped to latest
    Also refactored to use new pattern for NodeJS rest apis

--- services/users/src/.generated/rest/UsersRoutes.ts
+++ services/users/src/.generated/rest/UsersRoutes.ts
@@ -1,13 +1,70 @@
 //
 // GENERATED SOURCE - DO NOT EDIT
 //
-import type { RequestHandler } from 'express';
+import type { Request, Response } from 'express';
 import { UserRegistration } from 'generated:entities/UserRegistration';
 import { UserActivation } from 'generated:entities/UserActivation';
 import { UserSession } from 'generated:entities/UserSession';
 import { UserAuthentication } from 'generated:entities/UserAuthentication';
 import { PasswordChangeRequest } from 'generated:entities/PasswordChangeRequest';
 import { User } from 'generated:entities/User';
+
+export type RegisterUserRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    void,
+    void,
+    UserRegistration,
+    void,
+    Locals
+>;
+export type RegisterUserResponse<Locals extends Record<string, any> = Record<string, any>> = Response<void, Locals>;
+
+export type ActivateUserRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    void,
+    void,
+    UserActivation,
+    void,
+    Locals
+>;
+export type ActivateUserResponse<Locals extends Record<string, any> = Record<string, any>> = Response<void, Locals>;
+
+export type AuthenticationUserRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    void,
+    UserSession,
+    UserAuthentication,
+    void,
+    Locals
+>;
+export type AuthenticationUserResponse<Locals extends Record<string, any> = Record<string, any>> = Response<
+    UserSession,
+    Locals
+>;
+
+export type ResetPasswordRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    void,
+    void,
+    void,
+    { email: string },
+    Locals
+>;
+export type ResetPasswordResponse<Locals extends Record<string, any> = Record<string, any>> = Response<void, Locals>;
+
+export type ChangePasswordRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    void,
+    void,
+    PasswordChangeRequest,
+    void,
+    Locals
+>;
+export type ChangePasswordResponse<Locals extends Record<string, any> = Record<string, any>> = Response<void, Locals>;
+
+export type GetUserRequest<Locals extends Record<string, any> = Record<string, any>> = Request<
+    { id: string },
+    User,
+    void,
+    void,
+    Locals
+>;
+export type GetUserResponse<Locals extends Record<string, any> = Record<string, any>> = Response<User, Locals>;
 
 /**
  * Defines the methods and routes for the users API
@@ -17,35 +74,38 @@ export interface UsersRoutes<Locals extends Record<string, any> = Record<string,
      * Register new user
      * HTTP: POST /register
      */
-    registerUser: RequestHandler<void, void, UserRegistration, void, Locals>;
+    registerUser(req: RegisterUserRequest<Locals>, res: RegisterUserResponse<Locals>): void | Promise<void>;
 
     /**
      * Activate user registration
      * HTTP: POST /activate
      */
-    activateUser: RequestHandler<void, void, UserActivation, void, Locals>;
+    activateUser(req: ActivateUserRequest<Locals>, res: ActivateUserResponse<Locals>): void | Promise<void>;
 
     /**
      * Authenticate user
      * HTTP: POST /authenticate
      */
-    authenticationUser: RequestHandler<void, UserSession, UserAuthentication, void, Locals>;
+    authenticationUser(
+        req: AuthenticationUserRequest<Locals>,
+        res: AuthenticationUserResponse<Locals>
+    ): void | Promise<void>;
 
     /**
      * Reset password for user
      * HTTP: POST /reset_password
      */
-    resetPassword: RequestHandler<void, void, void, { email: string }, Locals>;
+    resetPassword(req: ResetPasswordRequest<Locals>, res: ResetPasswordResponse<Locals>): void | Promise<void>;
 
     /**
      * Change password for user
      * HTTP: POST /change_password
      */
-    changePassword: RequestHandler<void, void, PasswordChangeRequest, void, Locals>;
+    changePassword(req: ChangePasswordRequest<Locals>, res: ChangePasswordResponse<Locals>): void | Promise<void>;
 
     /**
      * Get user by id
      * HTTP: GET /users/:id
      */
-    getUser: RequestHandler<{ id: string }, User, void, void, Locals>;
+    getUser(req: GetUserRequest<Locals>, res: GetUserResponse<Locals>): void | Promise<void>;
 }
