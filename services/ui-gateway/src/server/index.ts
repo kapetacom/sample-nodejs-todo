16cc1a8213553e07bb6450c48971a6d8b955afdd
Morten Holm <mohoholm@gmail.com>
2024-02-23 14:18:52 +0100

    Revert

--- services/ui-gateway/src/server/index.ts
+++ services/ui-gateway/src/server/index.ts
@@ -1,6 +1,5 @@
 import { ConfigProvider, runApp } from '@kapeta/sdk-config';
 import { createServer } from './server';
-import { create as createHandlebars } from 'express-handlebars';
 import express from 'express';
 import { createRoutes } from './.generated/routes';
 import Path from 'node:path';
@@ -19,29 +18,16 @@ runApp(async (configProvider: ConfigProvider) => {
     // Create the server - see src/server/server.ts for more information
     const server = createServer(configProvider);
 
-    // Will serve the html, css and JS rendered by the build process
-    // In development mode, this will be using hot-reload and be served from memory
-    const webpackConfig = require('../../webpack.development.config');
-    server.configureFrontend(DIST_DIR, webpackConfig);
-
     // Serve static files from the assets directory
     server.express().use('/assets', express.static(ASSETS_DIR));
 
     // Includes the generated routes for REST and Web Fragment resources
     server.use(await createRoutes(configProvider));
 
-    // Add a catch-all route to render the main template
-    server.use(async (req, res, next) => {
-        // render the main template e.g. templates/main.hbs
-        await res.renderPage('main');
-    });
-
-    // Add app error handler
-    server.express().use(<express.ErrorRequestHandler>((err, _req, res, _next) => {
-        console.error(err);
-        res.status(err?.statusCode || err?.status || 500);
-        res.renderPage('main', { error: err });
-    }));
+    // Will serve the html, css and JS rendered by the build process
+    // In development mode, this will be using hot-reload and be served from memory
+    const webpackConfig = require('../../webpack.development.config');
+    server.configureFrontend(DIST_DIR, webpackConfig);
 
     server.start('web');
 }, BASE_DIR);
